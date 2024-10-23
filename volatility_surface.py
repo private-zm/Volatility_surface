7cc063a5067c88646622ab1e8570c17a37271770
private-zm <flowrythmic1@gmail.com>
2024-10-24 01:02:21 +0200

    Update volatility_surface.py

--- volatility_surface.py
+++ volatility_surface.py
@@ -8,6 +8,18 @@ from scipy.optimize import brentq
 from scipy.interpolate import griddata
 import plotly.graph_objects as go
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.title('Implied Volatility Surface')
 
